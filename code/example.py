41ee1ca91cbb13d9632bbf2810491ae37074fea8
Filippo <filippo.m.bianchi@uit.no>
2018-03-20 16:26:54 +0100

    some cleaning of the code

--- code/example.py
+++ code/example.py
@@ -21,31 +21,31 @@ config['n_drop'] = 5 # transient states to be dropped
 config['bidir'] = False # bidirectional or unidirectional reservoir
 
 # Dimensionality reduction parameters
-config['dimred_method'] ='tenpca'
-config['n_dim'] = 75
+config['dimred_method'] ='tenpca' # options: {None (no dimensionality reduction), 'pca', 'tenpca'}
+config['n_dim'] = 75 #  number of resulting dimensions after the dimensionality reduction procedure
 
 # Type of MTS representation
-config['mts_rep'] = 'reservoir'
-config['w_ridge_embedding'] = 5.0
+config['mts_rep'] = 'reservoir' # MTS representation:  {'last', 'output', 'reservoir'}
+config['w_ridge_embedding'] = 5.0 # regularization parameter of the ridge regression
 
 # Type of readout
-config['readout_type'] = 'lin'
+config['readout_type'] = 'lin' # readout used for classification: {'lin', 'mlp', 'svm'}
 
 # Linear readout parameters
-config['w_ridge'] = 1.0
+config['w_ridge'] = 1.0 # regularization of the ridge regression readout
 
 # MLP readout parameters
-config['mlp_layout'] = [20,20]
+config['mlp_layout'] = [20,20] # neurons in each MLP layer
 config['batch_size'] = 25 # samples in the mini-batches in gradient descent training
 config['num_epochs'] = 2000 # number of epochs 
-config['p_drop'] = 0.9
-config['w_l2'] = 0.0001
-config['learning_rate'] = 0.001
-config['nonlinearity'] = 'relu'
+config['p_keep'] = 0.9 # probability of keeping connections in dropout
+config['w_l2'] = 0.0001 # weight of the L2 regularization
+config['learning_rate'] = 0.001 # learning rate in the gradient descent optimization
+config['nonlinearity'] = 'relu' # type of activation function {'relu', 'tanh', 'maxout', 'kaf'}
 
 # SVM readout
-config['svm_gamma'] = 1.0
-config['svm_C'] = 1.0
+config['svm_gamma'] = 1.0 # bandwith of the RBF kernel
+config['svm_C'] = 1.0 # regularization for SVM hyperplane
 
 print(config)
 
@@ -90,7 +90,7 @@ accuracy, f1, tot_time =  train_RC_classifier(X=X,
                                               fc_layout=config['mlp_layout'],
                                               batch_size=config['batch_size'],
                                               num_epochs=config['num_epochs'],
-                                              p_drop=config['p_drop'],
+                                              p_keep=config['p_keep'],
                                               w_l2=config['w_l2'],
                                               learning_rate=config['learning_rate'],
                                               nonlinearity=config['nonlinearity'], 
